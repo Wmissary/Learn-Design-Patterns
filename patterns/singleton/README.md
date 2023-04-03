e6de87dbacb103228e2882d518806af2ec1c919b
Wmissary <pro.wmissary@gmail.com>
2023-04-04 01:21:51 +0200

    feat: add Singleton pattern

---
+++ patterns/singleton/README.md
@@ -0,0 +1,11 @@
+# 🎓 Singleton Design Pattern 🎓
+
+This project demonstrates the Singleton design pattern.
+
+## Problem 🤔
+
+We want to create a shopping cart for our e-commerce website, but we want to ensure that there is only one instance of the shopping cart throughout the website, so that all the products that the user adds to their cart are saved in a single cart instance.
+
+## Solution 💡
+
+The Singleton pattern ensures that a class has only one instance, and provides a global point of access to that instance. The shopping cart will be implemented as a Singleton, so that there is only one instance of the cart throughout the website.
---
+++ patterns/singleton/index.test.js
@@ -0,0 +1,90 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+import { ShoppingCart, shoppingCart2, shoppingCart3 } from "./index.js";
+
+describe("Singleton first solution", () => {
+  it("should be the same instance", () => {
+    const cart1 = new ShoppingCart();
+    const cart2 = new ShoppingCart();
+    assert.strictEqual(cart2, cart1);
+  });
+
+  it("should have the same items", () => {
+    const cart1 = new ShoppingCart();
+    const cart2 = new ShoppingCart();
+    cart1.addItem({ name: "foo", price: 10 });
+    cart2.addItem({ name: "bar", price: 20 });
+    assert.deepStrictEqual(cart2.getItems(), cart1.getItems());
+  });
+
+  it("should have the same total", () => {
+    const cart1 = new ShoppingCart();
+    const cart2 = new ShoppingCart();
+    cart1.addItem({ name: "foo", price: 10 });
+    cart2.addItem({ name: "bar", price: 20 });
+    assert.strictEqual(cart2.getTotal(), cart1.getTotal());
+  });
+
+  it("should have the same total after removing an item", () => {
+    const cart1 = new ShoppingCart();
+    const cart2 = new ShoppingCart();
+    cart1.addItem({ name: "foo", price: 10 });
+    cart2.addItem({ name: "bar", price: 20 });
+    cart1.removeItem({ name: "foo", price: 10 });
+    cart2.removeItem({ name: "bar", price: 20 });
+    assert.strictEqual(cart2.getTotal(), cart1.getTotal());
+  });
+});
+
+describe("Singleton second solution", () => {
+  it("should be the same instance", () => {
+    assert.strictEqual(shoppingCart2, shoppingCart2);
+  });
+
+  it("should have the same items", () => {
+    shoppingCart2.addItem({ name: "foo", price: 10 });
+    shoppingCart2.addItem({ name: "bar", price: 20 });
+    assert.deepStrictEqual(shoppingCart2.getItems(), shoppingCart2.getItems());
+  });
+
+  it("should have the same total", () => {
+    shoppingCart2.addItem({ name: "foo", price: 10 });
+    shoppingCart2.addItem({ name: "bar", price: 20 });
+    assert.strictEqual(shoppingCart2.getTotal(), shoppingCart2.getTotal());
+  });
+
+  it("should have the same total after removing an item", () => {
+    shoppingCart2.addItem({ name: "foo", price: 10 });
+    shoppingCart2.addItem({ name: "bar", price: 20 });
+    shoppingCart2.removeItem({ name: "foo", price: 10 });
+    shoppingCart2.removeItem({ name: "bar", price: 20 });
+    assert.strictEqual(shoppingCart2.getTotal(), shoppingCart2.getTotal());
+  });
+});
+
+describe("Singleton third solution", () => {
+  it("should be the same instance", () => {
+    assert.strictEqual(shoppingCart3, shoppingCart3);
+  });
+
+  it("should have the same items", () => {
+    shoppingCart3.addItem({ name: "foo", price: 10 });
+    shoppingCart3.addItem({ name: "bar", price: 20 });
+    assert.deepStrictEqual(shoppingCart3.getItems(), shoppingCart3.getItems());
+  });
+
+  it("should have the same total", () => {
+    shoppingCart3.addItem({ name: "foo", price: 10 });
+    shoppingCart3.addItem({ name: "bar", price: 20 });
+    assert.strictEqual(shoppingCart3.getTotal(), shoppingCart3.getTotal());
+  });
+
+  it("should have the same total after removing an item", () => {
+    shoppingCart3.addItem({ name: "foo", price: 10 });
+    shoppingCart3.addItem({ name: "bar", price: 20 });
+    shoppingCart3.removeItem({ name: "foo", price: 10 });
+    shoppingCart3.removeItem({ name: "bar", price: 20 });
+    assert.strictEqual(shoppingCart3.getTotal(), shoppingCart3.getTotal());
+  });
+});
